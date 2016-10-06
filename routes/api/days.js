f1bfce5f0b7a703b7efd2265d431b1b24928cda3
Clement Mihailescu <clementmihailescu@gmail.com>
2016-10-06 12:14:52 -0400

    We're getting there, Mike.

--- routes/api/days.js
+++ routes/api/days.js
@@ -7,12 +7,70 @@ const Restaurant = require('../../models/restaurant.js');
 const Activity = require('../../models/activity.js');
 const Day = require('../../models/day.js');
 
-router.get('', function (req, res, next) {
+// DAY Stuff
+
+router.get('', function(req, res, next) {
   Day
     .findAll()
     .then(days => {
       res.json(days);
-    });
+    })
+    .catch(next);
 });
+
+router.get('/:id', function(req, res, next) {
+  var id = req.params.id;
+  Day.findOne({where: {number: id}})
+    .then(day => {
+      res.json(day);
+    })
+    .catch(next);
+})
+
+router.delete('/:id', function(req, res, next) {
+  var id = req.params.id;
+  Day.destroy({where: {number: id}})
+  .then(something => {
+    console.log("You've just destroyed a day!");
+  })
+  .catch(next);
+})
+
+// Restaurant Stuff
+
+router.post('/:id/restaurants', function(req, res, next) {
+  var dayId = req.params.id;
+  var restaurantId = req.body.id;
+  Day.findOne({where: {number: restaurantId}})
+    .then(day => {
+      day.addRestaurant(restaurantId);
+    })
+    .catch(next);
+})
+
+router.post('/:id', function(req, res, next) {
+  var id = req.params.id;
+  Day.create({
+    number: id
+  })
+  .then(day => {
+    res.json(day);
+  })
+  .catch(next);
+})
+
+router.delete('/:id/restaurants', function(req, res, next) {
+  var dayId = req.params.id;
+  var restaurantId = req.body.id;
+  Day.findOne({where: {number: id}})
+    .then(day => {
+      day.removeRestaurant(restaurantId);
+    })
+    .catch(next);
+})
+
+
+
+
 
 module.exports = router;
